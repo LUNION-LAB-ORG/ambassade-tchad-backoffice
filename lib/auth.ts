6e49d449f3b6f79e4a7fc95eb5ad2f546abb4119
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-09-09 13:58:53 +0000

    mise à jour du middleware

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,6 +8,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60, // 30 jours (refresh token)
   },
+  secret: process.env.AUTH_SECRET,
   trustHost: true,
   debug: process.env.NODE_ENV === "development",
   providers: [
