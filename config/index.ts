1c811702fc7219193563582fd1811c1466b64283
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-09-09 14:12:32 +0000

    mise à jour de la structure

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,4 @@
 export const locales = ['en', 'ar', "fr"];
 
-export const baseURL = process.env.NEXT_PUBLIC_API_BACKEND_URL || "http://localhost:8083/api/v1";
+export const baseURL = process.env.NEXT_PUBLIC_API_BACKEND_URL || "http://localhost:8081/api/v1";
+export const baseFileURL = process.env.NEXT_PUBLIC_API_FILE_URL || "http://localhost:8081";
